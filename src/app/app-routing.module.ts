04cd7851848363ba44127f5841ef0d9e61b88060
Alex Ssanya <alexssanya@gmail.com>
2019-08-19 23:09:49 +0300

    add wild path handler

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component'
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
