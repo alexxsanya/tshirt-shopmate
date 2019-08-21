664f776e2bd1dcfe1d607501b4cf4dd961ef13c5
Alex Ssanya <alexssanya@gmail.com>
2019-08-21 23:02:54 +0300

    add banner background image

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -9,7 +9,8 @@ export class BannerComponent implements OnInit {
 
   banner_title: string;
   banner_categories: Array<String>;
-
+  urlToImage: object;
+  
   constructor() { 
     this.banner_title = "Mens Wear"
     this.banner_categories = new Array (
@@ -25,9 +26,12 @@ export class BannerComponent implements OnInit {
       'Jewelly',
       'Joggers',
       'Jumpers & Cardigans'
-    )
+    );
+    this.urlToImage = {
+      female: "https://www.pngarts.com/files/1/Fashion-Transparent-Images.png",
+      male: "https://www.pngarts.com/files/5/Black-Blazer-PNG-High-Quality-Image.png",
+    };
   }
-
   ngOnInit() {
   }
 
