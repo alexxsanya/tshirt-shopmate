df610d73f8f85de39670d4fe01ac4d1045be3eff
Alex Ssanya <alexssanya@gmail.com>
2019-09-07 15:58:47 +0300

    add product info cover

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -8,10 +8,13 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProductComponent implements OnInit {
   @Input() name;
   @Input() thumbnail;
-  
+  isHovered: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  showInfo(event: Event){
+    this.isHovered = this.isHovered?false:true;
+  }
 }
