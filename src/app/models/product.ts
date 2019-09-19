b007213823211ffa9bf3ad651b5fbafb1e325329
Alex Ssanya <alexssanya@gmail.com>
2019-09-19 21:56:03 +0300

    add product model

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -7,4 +7,12 @@ export interface Size {
     name: string,
     status: string
 }
-  
+
+export interface Product {
+    product_id: number,
+    name: string,
+    description: string,
+    price: string,
+    discounted_price: string,
+    thumbnail: string
+}
