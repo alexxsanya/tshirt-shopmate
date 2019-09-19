0b719d1ccb83fed73b16e22cd34430761a88706d
Alex Ssanya <alexssanya@gmail.com>
2019-09-19 21:56:46 +0300

    add product service

---
+++ src/app/services/products/products.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from 'src/app/models/product';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private http: HttpClient) { }
+
+  async getProducts(limit): Promise<Product> {
+    return await this.http.get<any>(`${environment.api_url}/products?limit=${limit}`)
+    .toPromise();
+  }
+
+}
