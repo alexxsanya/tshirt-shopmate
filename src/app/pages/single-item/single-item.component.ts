128af8106bfaf47b8fd9b71a0b01402bde73c391
Alex Ssanya <alexssanya@gmail.com>
2019-09-27 11:48:04 +0300

    update single-item component with live data

--- src/app/pages/single-item/single-item.component.ts
+++ src/app/pages/single-item/single-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-single-item',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleItemComponent implements OnInit {
   rating: number = 2;
-  constructor() { }
+  quantity: number = 1;
+  product_id: string;
+  productDetails: any = {};
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute, 
+    private productsService: ProductsService,
+    private ngxService: NgxUiLoaderService,
+    ) { }
+
+  async ngOnInit() {
+    this.product_id = this.route.snapshot.paramMap.get('product_id');
+
+    this.ngxService.startLoader('loader-product'); 
+
+    this.productDetails = await this.getAProduct(this.product_id);
+    this.productDetails.rating = Math.floor(Math.random() * 5) + 1;
+
+    setTimeout(() => {
+      this.ngxService.stopLoader('loader-product'); 
+    }, 2000);
+
+  }
+
+  decrementQty = () => {
+    this.quantity = this.quantity < 2 ? 1: this.quantity -= 1;
+  }
+
+  incrementQty = () => {
+    this.quantity +=1
+  }
+
+  addToCart = () => {
+    console.log("added to cart");
+  }
+
+  getAProduct(product_id){
+    let products: any = []
+
+    products = this.productsService.getAProduct(product_id).then(productsData => {
+      return productsData;
+    });
+
+    return products;
   }
 
 }
