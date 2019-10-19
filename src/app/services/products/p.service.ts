9e8f3deca0f78bbb59df1002832dc5c29ee78763
alexxsanya <alexxsanya@gmail.com>
2019-10-19 17:44:54 +0300

    add temp service to support ngrx

---
+++ src/app/services/products/p.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from 'src/app/models/product';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(limit): Observable<Product[]> {
+    return this.http.get<Product[]>(`${environment.api_url}/products`);
+  }
+
+  getProductById(productID: number): Observable<Product> {
+    return this.http.get<Product>(`${environment.api_url}/products/${productID}`);
+  }
+
+}
