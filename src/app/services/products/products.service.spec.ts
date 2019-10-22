7a6ed2ddd19acabe1d1e07e968d8036a4e94fbd3
alexxsanya <alexxsanya@gmail.com>
2019-10-23 01:21:25 +0300

    update dependency path

--- src/app/services/products/products.service.spec.ts
+++ src/app/services/products/products.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProductsService } from './products.service';
+import { ProductsService } from './p.service';
 
 describe('ProductsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
