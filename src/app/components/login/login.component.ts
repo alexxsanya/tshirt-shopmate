da9e4aa71b0c8e8c617779ddcf269b34921a1962
alexxsanya <alexxsanya@gmail.com>
2019-10-28 04:08:05 +0300

    implement login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import * as LoginActions from './state/login.actions';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  loginForm: FormGroup;
+  userStore:any = {};
+  constructor(public fb: FormBuilder, private store: Store<any>) {
+    this.loginForm = this.fb.group({
+      loginEmail: [''],
+      loginPass: [null]
+    });
   }
 
+  ngOnInit() {
+
+  }
+
+  loginUser = () => {
+    const formData: any = new FormData();
+    formData.append('email', this.loginForm.get('loginEmail').value);
+    formData.append('password', this.loginForm.get('loginPass').value);
+
+    this.store.dispatch(new LoginActions.LoadLogin(formData));
+    this.store.subscribe((state) => {
+      this.userStore = state.user.user;
+      console.log(this.userStore);
+    });
+
+  }
 }
