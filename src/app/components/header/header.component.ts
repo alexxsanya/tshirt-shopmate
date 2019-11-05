db8cb4d8d20f42a19734abb0c9dcfac6eb57bbaa
alexxsanya <alexxsanya@gmail.com>
2019-11-06 01:49:45 +0300

    set login/signup to show when not logged in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { LocalStorageService } from 'src/app/shared/util/localStorageService';
+import { access } from 'fs';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +10,22 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class HeaderComponent implements OnInit {
   searchForm: FormGroup;
+  isLoggedIn = false;
 
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder, private localStorage: LocalStorageService) {
     this.searchForm = this.fb.group({
       search: [''],
     });
   }
   ngOnInit() {
+    const accessToken = this.localStorage.get('accessToken',undefined);
+
+    if ( accessToken !== undefined) {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
+
   }
 
 }
