47e523d06927cd5102aff76890b28972a43ec844
alexxsanya <alexxsanya@gmail.com>
2019-11-06 02:02:03 +0300

    update the login function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import * as LoginActions from './state/login.actions';
+import { LocalStorageService } from 'src/app/shared/util/localStorageService';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { WindowService } from 'src/app/shared/util/windowService';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +13,13 @@ import * as LoginActions from './state/login.actions';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  userStore:any = {};
-  constructor(public fb: FormBuilder, private store: Store<any>) {
+  userStore: any = {};
+  constructor(public fb: FormBuilder,
+              private store: Store<any>,
+              private localStorage: LocalStorageService,
+              private ngxService: NgxUiLoaderService,
+              private window: WindowService,
+  ) {
     this.loginForm = this.fb.group({
       loginEmail: [''],
       loginPass: [null]
@@ -23,14 +31,22 @@ export class LoginComponent implements OnInit {
   }
 
   loginUser = () => {
-    const formData: any = new FormData();
-    formData.append('email', this.loginForm.get('loginEmail').value);
-    formData.append('password', this.loginForm.get('loginPass').value);
+    this.ngxService.startLoader('loader-login');
+    const data = {
+      email: this.loginForm.get('loginEmail').value,
+      password: this.loginForm.get('loginPass').value
+    };
 
-    this.store.dispatch(new LoginActions.LoadLogin(formData));
+    this.store.dispatch(new LoginActions.LoadLogin(data));
     this.store.subscribe((state) => {
       this.userStore = state.user.user;
-      console.log(this.userStore);
+      if (this.userStore.accessToken !== undefined) {
+        this.localStorage.set('accessToken', this.userStore.accessToken);
+        this.window.reload();
+      }
+      setTimeout(() => {
+        this.ngxService.stopLoader('loader-login');
+      }, 2000);
     });
 
   }
