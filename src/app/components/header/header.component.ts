e7b644bd323da215ea9aafc50a7d140e5940a3e3
alexxsanya <alexxsanya@gmail.com>
2019-11-07 00:01:17 +0300

    add logout button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { LocalStorageService } from 'src/app/shared/util/localStorageService';
 import { access } from 'fs';
+import { WindowService } from 'src/app/shared/util/windowService';
 
 @Component({
   selector: 'app-header',
@@ -12,20 +13,25 @@ export class HeaderComponent implements OnInit {
   searchForm: FormGroup;
   isLoggedIn = false;
 
-  constructor(public fb: FormBuilder, private localStorage: LocalStorageService) {
+  constructor(public fb: FormBuilder, private localStorage: LocalStorageService, private window: WindowService) {
     this.searchForm = this.fb.group({
       search: [''],
     });
   }
   ngOnInit() {
-    const accessToken = this.localStorage.get('accessToken',undefined);
+    const accessToken = this.localStorage.get('accessToken', undefined);
 
     if ( accessToken !== undefined) {
       this.isLoggedIn = true;
     } else {
       this.isLoggedIn = false;
     }
+    console.log(`2. access: ${accessToken} - loggedin: ${this.isLoggedIn}`);
+  }
 
+  logout() {
+    this.localStorage.clear();
+    this.window.reload();
   }
 
 }
