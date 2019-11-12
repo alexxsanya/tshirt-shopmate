4c95ee3f4d3bd4b24cdd63f1a695112554b71520
alexxsanya <alexxsanya@gmail.com>
2019-11-13 00:25:04 +0300

    update cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -14,16 +14,29 @@ import { WindowService } from 'src/app/shared/util/windowService';
 export class CartComponent implements OnInit {
   loginForm: FormGroup;
   userStore: any = {};
+  cartCount = 1;
+  cartItems = [{
+    id: 1,
+    name: 'Blue Tshirt',
+    price: '15',
+    qty: 1,
+    thumbnail: 'https://www.jumia.ug/images/local/placeholder/placeholder_xs_1.jpg'
+  }, {
+    id: 1,
+    name: 'Black Tshirt',
+    price: '55',
+    qty: 2,
+    thumbnail: 'https://www.jumia.ug/images/local/placeholder/placeholder_xs_1.jpg'
+  },
+];
+
   constructor(public fb: FormBuilder,
               private store: Store<any>,
               private localStorage: LocalStorageService,
               private ngxService: NgxUiLoaderService,
               private window: WindowService,
   ) {
-    this.loginForm = this.fb.group({
-      loginEmail: [''],
-      loginPass: [null]
-    });
+    this.cartCount = this.cartItems.length;
   }
 
   ngOnInit() {
