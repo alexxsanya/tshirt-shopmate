f496f481bcb224e18d9b31e900e4d472fe2f50be
alexxsanya <alexxsanya@gmail.com>
2019-11-18 12:39:52 +0300

    add token validator

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import * as jwt_decode from 'jwt-decode';
+import { LocalStorageService } from 'src/app/shared/util/localStorageService';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private localStorage: LocalStorageService) {}
 
   loginUser(userFormData): Observable<any> {
     return this.http.post(`${environment.api_url}/customers/login`, userFormData);
@@ -17,5 +19,29 @@ export class UserService {
   createUser(userFormData): Observable<any> {
     console.log(userFormData);
     return this.http.post(`${environment.api_url}/customers`, userFormData);
+  }
+
+  getTokenExpirationDate(token: string): Date {
+    const decoded = jwt_decode(token);
+
+    if (decoded.exp === undefined) { return null; }
+
+    const date = new Date(0);
+    date.setUTCSeconds(decoded.exp);
+    return date;
+  }
+
+  isTokenExpired = (token?: string): boolean => {
+    if (token === undefined) { token = this.getToken(); }
+    if (token === undefined) { return true; }
+
+    console.log('auth - ' + token);
+    const date = this.getTokenExpirationDate(token);
+    if (date === undefined) { return false; }
+    return !(date.valueOf() > new Date().valueOf());
+  }
+
+  getToken = () => {
+    return this.localStorage.get('accessToken', undefined);
   }
 }
