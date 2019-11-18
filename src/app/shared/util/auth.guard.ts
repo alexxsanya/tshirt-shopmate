45415c7ef3c703ec2a293ae94020983cb218a70d
alexxsanya <alexxsanya@gmail.com>
2019-11-18 13:03:26 +0300

    update the auth guard service

--- src/app/shared/util/auth.guard.ts
+++ src/app/shared/util/auth.guard.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { UserService } from 'src/app/services/user/user.service';
+import { WindowService } from './windowService';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor(
     private router: Router,
-    private authService: UserService) {}
+    private authService: UserService,
+    private window: WindowService) {}
 
   canActivate() {
     if (!this.authService.isTokenExpired()) {
-      console.log('Token is expired')
       return true;
     }
-
-    this.router.navigate(['/login']);
+    this.window.alert('session expired, please login again');
+    this.authService.logOut();
+    // this.router.navigate(['/login']);
     return false;
   }
 
