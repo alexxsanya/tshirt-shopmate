92648982baa07755eabee3045ec7f0aa8106d570
alexxsanya <alexxsanya@gmail.com>
2019-11-18 13:01:40 +0300

    add alert & goto function

--- src/app/shared/util/windowService.ts
+++ src/app/shared/util/windowService.ts
@@ -7,10 +7,17 @@ import { isPlatformBrowser } from '@angular/common';
 export class WindowService {
   constructor(@Inject(PLATFORM_ID) private platformId: object) { }
 
-  reload() {
+  reload = () => {
     if (isPlatformBrowser(this.platformId)) {
       window.location.reload();
     }
   }
 
+  alert = (message?: string) => {
+    alert(message);
+  }
+
+  goto = (url: string) => {
+    window.location.href = `/${url}`;
+  }
 }
